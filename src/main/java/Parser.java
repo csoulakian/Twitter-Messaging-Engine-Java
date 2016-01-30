6f9683ab7413a9e718cd6ef1433d507d6cc375d3
Chrissy Soulakian <csoulakian@luc.edu>
2016-01-29 22:52:06 -0600

    created empty Reader and Parser classes with class comments

---
+++ src/main/java/Parser.java
@@ -0,0 +1,12 @@
+/**
+ * Chrissy Soulakian
+ * twitter-msg-engine Project
+ * Created on 1/29/2016
+ *
+ * Parses a list of strings that represent each "word" in the original message.
+ * Checks for @ or # at beginning of string, adds the rest of that string to a HashMap.
+ * HashMap will contain mentions, topics, and URLs.
+ */
+public class Parser {
+
+}
---
+++ src/main/java/Reader.java
@@ -0,0 +1,10 @@
+/**
+ * Chrissy Soulakian
+ * twitter-msg-engine Project
+ * Created on 1/29/2016
+ *
+ * Reads in user input using scanner and adds each word to a list.
+ */
+public class Reader {
+
+}
