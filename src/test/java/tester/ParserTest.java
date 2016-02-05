f5ac57d679001555e9806c6ee28ae7c8f93d32a3
Chrissy Soulakian <csoulakian@luc.edu>
2016-02-04 18:02:31 -0600

    created parser test class

---
+++ src/test/java/tester/ParserTest.java
@@ -0,0 +1,21 @@
+package tester;
+
+import org.junit.Test;
+import twitter.Parser;
+
+/**
+ * Chrissy Soulakian
+ * twitter-msg-engine Project
+ * Created on 2/4/2016
+ */
+public class ParserTest {
+
+    @Test
+    public void basicParse() {
+    }
+
+
+}
+
+
+
