733cf59a0e26624c26eaf68457f08f2c3deb8a25
Chrissy Soulakian <csoulakian@luc.edu>
2016-02-08 14:30:25 -0600

    added a ParserTest for empty input, with only spaces, and when message is too long.

--- src/test/java/tester/ParserTest.java
+++ src/test/java/tester/ParserTest.java
@@ -11,11 +11,26 @@ import twitter.Parser;
 public class ParserTest {
 
     @Test
+    public void emptyParse() {
+        //empty message
+        Parser p1 = new Parser(TestFixtures.L0);
+        assert(p1.parsedMap.isEmpty());
+
+        //message with only spaces
+        Parser p2 = new Parser(TestFixtures.L00);
+        assert(p2.parsedMap.isEmpty());
+
+        //message is too long
+        Parser p3 = new Parser(TestFixtures.L000);
+        assert(p3.parsedMap.isEmpty());
+    }
+
+    @Test
     public void basicParse() {
-        Parser p = new Parser(TestFixtures.L1);
-        assert(p.parsedMap.get("mentions").size() == 0);
-        assert(p.parsedMap.get("topics").size() == 0);
-        assert(p.parsedMap.get("urls").size() == 0);
+        Parser p1 = new Parser(TestFixtures.L1);
+        assert(p1.parsedMap.get("mentions").size() == 0);
+        assert(p1.parsedMap.get("topics").size() == 0);
+        assert(p1.parsedMap.get("urls").size() == 0);
     }
 
 
--- src/test/java/tester/TestFixtures.java
+++ src/test/java/tester/TestFixtures.java
@@ -14,44 +14,44 @@ public class TestFixtures {
     //test messages
     static String m0 = "";
     static String m00 = " ";
-    static String m1 = "one TWO Three, this is a basic tweet!";
-    static String m2 = "this tweet is way tooooooooooooooooooooooooooooooooooooooooooooo long, " +
+    static String m000 = "this tweet is way tooooooooooooooooooooooooooooooooooooooooooooo long, " +
             "because it is way tooooooooooooooooooo many characters blah blah blah " +
             "how do we handle this long tweet?";
-    static String m3 = "a ";
-    static String m4 = "This is a statement. Can you handle the questions?";
-    static String m5 = "Hey @John, have you heard from our friend, @Martha?";
-    static String m6 = "@john";
-    static String m7 = "@";
-    static String m8 = "@.";
-    static String m9 = "@#";
-    static String m10 = "Have you been #tagged yet?";
-    static String m11 = "@John believes #Google is cool.";
-    static String m12 = "@Billy is #cooler than @John";
-    static String m13 = "#there #are #a #lot #of #tags @here";
-    static String m14 = "http://www.google.com is the best website ever!";
-    static String m15 = "my #favorite website is www.google.com";
-    static String m16 = "@Billy #loves www.google.com";
+    static String m1 = "one TWO Three, this is a basic tweet!";
+    static String m2 = "a ";
+    static String m3 = "This is a statement. Can you handle the questions?";
+    static String m4 = "Hey @John, have you heard from our friend, @Martha?";
+    static String m5 = "@john";
+    static String m6 = "@";
+    static String m7 = "@.";
+    static String m8 = "@#";
+    static String m9 = "Have you been #tagged yet?";
+    static String m10 = "@John believes #Google is cool.";
+    static String m11 = "@Billy is #cooler than @John";
+    static String m12 = "#there #are #a #lot #of #tags @here";
+    static String m13 = "http://www.google.com is the best website ever!";
+    static String m14 = "my #favorite website is www.google.com";
+    static String m15 = "@Billy #loves www.google.com";
 
     //string arrays for the messages
     static String[] L0 = {};
     static String[] L00 = {};
+    static String[] L000 = {};
     static String[] L1 = {"one", "TWO", "Three,", "this", "is", "a", "basic", "tweet!"};
-    //static String[] L2 =
-    static String[] L3 = {"a"};
-    static String[] L4 = {"This", "is", "a", "statement.", "Can", "you", "handle", "the", "questions?"};
-    static String[] L5 = {"Hey", "@John,", "have", "you", "heard", "from", "our", "friend,", "@Martha?"};
-    static String[] L6 = {"@john"};
-    static String[] L7 = {"@"};
-    static String[] L8 = {"@."};
-    static String[] L9 = {"@#"};
-    static String[] L10 = {"Have", "you", "been", "#tagged", "yet?"};
-    static String[] L11 = {"@John", "believes", "#Google", "is", "cool."};
-    static String[] L12 = {"@Billy", "is", "#cooler", "than", "@John"};
-    static String[] L13 = {"#there", "#are", "#a", "#lot", "#of", "#tags", "@here"};
-    static String[] L14 = {"http://www.google.com", "is", "the", "best", "website", "ever!"};
-    static String[] L15 = {"my", "#favorite", "website", "is", "www.google.com"};
-    static String[] L16 = {"@Billy", "#loves", "www.google.com"};
+    static String[] L2 = {"a"};
+    static String[] L3 = {"This", "is", "a", "statement.", "Can", "you", "handle", "the", "questions?"};
+    static String[] L4 = {"Hey", "@John,", "have", "you", "heard", "from", "our", "friend,", "@Martha?"};
+    static String[] L5 = {"@john"};
+    static String[] L6 = {"@"};
+    static String[] L7 = {"@."};
+    static String[] L8 = {"@#"};
+    static String[] L9 = {"Have", "you", "been", "#tagged", "yet?"};
+    static String[] L10 = {"@John", "believes", "#Google", "is", "cool."};
+    static String[] L11 = {"@Billy", "is", "#cooler", "than", "@John"};
+    static String[] L12 = {"#there", "#are", "#a", "#lot", "#of", "#tags", "@here"};
+    static String[] L13 = {"http://www.google.com", "is", "the", "best", "website", "ever!"};
+    static String[] L14 = {"my", "#favorite", "website", "is", "www.google.com"};
+    static String[] L15 = {"@Billy", "#loves", "www.google.com"};
 
     //HashMaps of the messages
 
